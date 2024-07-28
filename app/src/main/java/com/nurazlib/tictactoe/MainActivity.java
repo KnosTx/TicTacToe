12bd8efece2af10a51f427b5064b26bf59990423
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-28 16:47:08 +0700

    Typo

--- app/src/main/java/com/nurazlib/tictactoe/MainActivity.java
+++ app/src/main/java/com/nurazlib/tictactoe/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         adView = findViewById(R.id.adView);
 
         AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest):
+        adView.loadAd(adRequest);
     }
 
     @Override
